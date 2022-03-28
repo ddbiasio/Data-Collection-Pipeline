00819150f88d88414e01ea3b1575bbf705267054
Siobhan <siobhanmdangelo@googlemail.com>
2022-03-28 19:32:53 +0100

    Milestone 3

--- README.md
+++ README.md
@@ -23,4 +23,28 @@ I have limited this to 2 pages for the time being to ensure I am not spending a 
 Added a call to get_data in the __init__ method.  The initialiser accepts the search string as input which is the passed to the search method to retrieve recipes based on a key word(s).
 Added if __name__ == "__main__" block to the code which creates an an instance of the class and returns the links to recipes from the first 2 pages of the search
 
+# Milestone 3
+Created a class for the recipe details to make the collection of details easier to read, and with the intention to use the __dict__ attribute to easily output the recipe to dictionary format
+## Tasks 1 and 2
+Created a method to return a deterministic IC and a UUID
+The last element of the recipe page URL is unique so this this used as the item ID
+I used uuid.uuid4() to generate a UUID
+
+## Task 3
+Created a method in the main class to extract data from a recipe page as identified by a URL passed to the function
+This loads the page and extracts ingredients, method, prep and cook times and nutritional info
+The recipe details are held in div (class='post recipe') so I find this element first and then subsequent sections are located relevant to this.  with some sections I noticed the class of items Iwanted to find was used in multiple sections so I would find the enclosing block first to be sure I was then looping through the correct elements for the information I needed to find
+
+## Task 4
+I used the __dict__ attribute of my recipe class to create a dictionary for the recipe
+
+## Task 5
+In my main program (after loading and searching for recipes) I loop through each page to get the recipe URLs.  Then for each URL I load the recipe page to extract the details.  As each recipe dictionary is created I update a dictionary (all_recipes) with each new recipe.  The final dictionary is them written to data.json in a folder ./raw_data/{search} where {search} is the search string used to find the recipes
+
+I discovered that UUIDs are not serializable so created a custome JSONDecoder for the cls parameter to provide a serializable version of the UUID
+
+## Task 6
+Each recipe card has a single image.  When building the recipe dictionary I get the image URL from the recipe details and use urllib.request.urlretrieve to extract the image and save to ./raw_data/{search}/images, naming it with the recipe ID e.g. chicken-pasta-bake.jpg so this can be linked to a recipe at a later stage
+
+
 
--- data_collector.py
+++ data_collector.py
@@ -1,10 +1,99 @@
-from re import S, search
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+import os
+import uuid
+import json
+import urllib.request
+
+class UUIDEncoder(json.JSONEncoder):
+    """
+    Contains a function to make UUID in dictionary serializable
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    default(obj : object) -> hex
+        If the object is a UUID then return it in hex format
+    
+    """
+    def default(self, obj: object) -> hex:
+        if isinstance(obj, uuid.UUID):
+            # if the obj is uuid, we simply return the value of uuid
+            return obj.hex
+        return json.JSONEncoder.default(self, obj)
+
+class recipe():
+    """
+    This class provides a means to store the recipe information whilst processing
+    
+ 
+    Attributes
+    ----------
+        recipe_id : str
+            The unique name for the recipe obtained from the URL
+        recipe_UUID : UUID
+            The UUIDv4 generated for the recipe
+        recipe_name : str
+            The name of the recipe
+        ingredients : list
+            A list of the ingredients
+        planning_info : dict
+            Information on preparation and cooking times
+        method : dict
+            A dictionary of the method with keys Step1, Step2, .. Stepn as required
+        nutritional_info : dict
+            A dictionary of the nutritional info e.g. calories, fat, sugar etc.
+        url : str
+            The URL for the recipe page
+        image_url : str
+            The URL for the recipe image
+        
+    Methods
+    -------
+        get_recipe_ids(self, url) -> tuple(str, UUID)
+            Returns the recipe unique name (from url) and a UUIDv4 for the recipe
+
+    """
+
+    def __init__(self):
+        self.recipe_id = ""
+        self.recipe_UUID = ""
+        self.recipe_name = ""
+        self.ingredients = []
+        self.planning_info = {}
+        self.method = {}
+        self.nutritional_info = {}
+        self.url = ""
+        self.image_url = ""
+
+    def get_recipe_ids(self, url: str):
+    #def get_recipe_ids(self, url: str) -> tuple(str, uuid.UUID):
+    #This throws an error when I add annotation and can't see why
+    #various articles suggest other annotation e.g. Tuple[x, y] or tuple[x, y]
+    #But Tuple is said to be deprecated and tuple[] shows as syntax error
+        """
+        Returns the recipe unique name (from url) and a UUIDv4 for the recipe
+
+        Parameters
+        ----------
+        url : str
+            The url for the recipe
+
+        Returns
+        -------
+        tuple(str, str)
+            The recipe name ID, and recipe UUIDv4
+        """
+        recipe_id = url.split('/')[-1]
+        recipe_uuid = uuid.uuid4()
+        return recipe_id, recipe_uuid
 
 class recipe_collector():
 
@@ -34,19 +123,16 @@ class recipe_collector():
         get_recipes(url : str) -> dict
             Retrieves data from the specified URL
 
-        def get_recipe_links(self, page_num): -> list
+        get_recipe_links(self, page_num: int): -> list
             Gets the URLs for each recipe on a given page
 
-        def get_recipe_data(self, url): -> dict
+        get_recipe_data(self, url: str): -> dict
             Gets the ingredients, method and other information for a single recipe
         
-        store_data_from_page() -> none
-            Stores scraped data in the data dictionary
-
-        go_to_page_num() -> bool
+        go_to_page_num(self, page_num: int): -> bool
             Navigates to the page in search results by page number
         
-        go_to_page_url() -> bool
+        go_to_page_url(self, url: str) -> bool
             Navigates to a page by its URL (usually for a specific recipe)
 
     """
@@ -61,8 +147,12 @@ class recipe_collector():
         self._base_url = "https://www.bbcgoodfood.com/"
         self.data_dictionary = {}
         self._driver = None
+        self.recipe_urls = []
 
-        self.get_data(search_string)
+        #initiate the session and accept cookies
+        self._driver = webdriver.Firefox()
+        self._driver.get(self._base_url)
+        self.accept_cookies()
     
     def accept_cookies(self) -> None:
         """
@@ -89,43 +179,11 @@ class recipe_collector():
             #if timeout to get the button then cookies must have been accepted before
             return
 
-    def get_data(self, search_string: str):
-        """
-        Main function to load the site and scrap recipe data based on a keyword search
-
-        Parameters
-        ----------
-        search_string : str
-            The ingredient, meal type or other relevant key word for the recipe search
-
-        """
-        try:
-
-            #initiate the session and accept cookies
-            self._driver = webdriver.Firefox()
-            self._driver.get(self._base_url)
-            self.accept_cookies()
-
-            #execute the search
-            if self.search_recipes(search_string):
-                
-                #initialise the list for the recipe URLs
-                recipe_list = []
-
-                for page in range(1, 3):
-                    recipe_list += self.get_recipe_links(page)
-                    #loop through items on page
-                    #go to each item and scrape data
-
-                print(f"Found {len(recipe_list)} recipes relating to {search_string}")
-                print(recipe_list)
-            else:
-                print(f"There were no recipes relating to {search_string}")
-           
-        except Exception as e:
-            print(str(e))
-
-        self._driver.close()
+        except StaleElementReferenceException:
+            #This is being thrown intermittently - there is not other navigation etc.
+            #which would case this so maybe something in page load.  Retry whrn this occurs
+            accept_button = WebDriverWait(self._driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//button[(@class=' css-1x23ujx')]")))
+            accept_button.click()     
 
     def search_recipes(self, search_string: str) -> bool:
         
@@ -157,7 +215,7 @@ class recipe_collector():
             #if the no results div does not exist then search returned results
             return True
 
-    def get_recipe_links(self, page_num) -> list:
+    def get_recipe_links(self, page_num) -> None:
 
         """
         Returns the URLs for each recipe on a given page
@@ -169,8 +227,7 @@ class recipe_collector():
 
         Returns
         -------
-        recipe_list : list
-            A list of all the URLs from the page
+        None
 
         """
         if self.go_to_page_num(page_num):
@@ -180,18 +237,15 @@ class recipe_collector():
                     by=By.XPATH, value='//a[(@class="body-copy-small standard-card-new__description")]'
                     )
 
-            recipe_links = []
-
             for idx, recipe in enumerate(recipes):
                 #go to each recipe and get the link and add to list
                 recipe_url = recipe.get_attribute("href")
-                recipe_links.append(recipe_url)
+                self.recipe_urls.append(recipe_url)
 
-            return recipe_links
         else:
             print(f"Unable to navigate to page {page_num} of search results")
 
-    def get_recipe_data(self, url: str):
+    def get_recipe_data(self, url: str) -> None:
         """
         Gets the ingredients, method and other information for a single recipe
 
@@ -201,24 +255,56 @@ class recipe_collector():
             The URL of the recipe
 
         Returns:
-            Dictionary containing the recipe data
+            None
 
         """
+        self._driver.get(url)
+        recipe_object = recipe()
 
-    def store_data_from_page(self):
-        """
-        Stores scraped data in the data dictionary
+        recipe_div = self._driver.find_element(by=By.XPATH, value="//div[(@class='post recipe')]")
 
-        Parameters
-        ----------
-        None
+        recipe_object.recipe_id, recipe_object.recipe_UUID = recipe_object.get_recipe_ids(url)
 
-        Returns
-        -------
-        None
+        recipe_name = recipe_div.find_element(by=By.XPATH, value=".//h1[(@class='heading-1')]")
+
+        ingredient_section = recipe_div.find_element(by=By.XPATH, value=".//section[(@class='recipe__ingredients col-12 mt-md col-lg-6')]")
+        ingredients_list = ingredient_section.find_elements(by=By.XPATH, value=".//li[(@class='pb-xxs pt-xxs list-item list-item--separator')]")
         
-        """
-        pass
+        for ingredient in ingredients_list:
+            recipe_object.ingredients.append(ingredient.text)
+        
+        method_section = recipe_div.find_element(by=By.XPATH, value=".//section[(@class='recipe__method-steps mb-lg col-12 col-lg-6')]")
+        method_steps = method_section.find_elements(by=By.XPATH, value=".//li[(@class='pb-xs pt-xs list-item')]")
+
+        for step in method_steps:
+            step_num = step.find_element(by=By.XPATH, value="./span[(@class='mb-xxs heading-6')]")
+            step_details = step.find_element(by=By.TAG_NAME, value="p")
+        
+            recipe_object.method.update({step_num.text: step_details.text})
+        
+        nutrition_items = recipe_div.find_elements(by=By.XPATH, value=".//tr[(@class='key-value-blocks__item')]")
+        
+        for item in nutrition_items:
+            nutrition_item = item.find_element(by=By.XPATH, value="./td[(@class='key-value-blocks__key')]").text
+            nutrition_value = item.find_element(by=By.XPATH, value="./td[(@class='key-value-blocks__value')]").text
+
+            recipe_object.nutritional_info.update({nutrition_item: nutrition_value})
+
+        planning_list = recipe_div.find_element(by=By.XPATH, value=".//div[(@class='icon-with-text time-range-list cook-and-prep-time post-header__cook-and-prep-time')]")
+
+        planning_items = planning_list.find_elements(by=By.XPATH, value=".//li[(@class='body-copy-small list-item')]")
+        for planning_item in planning_items:
+            planning_text = planning_item.find_element(by=By.XPATH, value=".//span[(@class='body-copy-bold mr-xxs')]").text
+            planning_time = planning_item.find_element(by=By.XPATH, value=".//time").text
+
+            recipe_object.planning_info.update({planning_text.replace(":", ""): planning_time})
+
+        recipe_object.url = url
+
+        recipe_object.image_url = recipe_div.find_element(by=By.XPATH, 
+            value=".//img[(@class='image__img')]").get_attribute('src').split('?', 1)[0]
+
+        return recipe_object.__dict__
 
     def go_to_page_num(self, page_num: int) -> bool:
         """
@@ -263,5 +349,75 @@ class recipe_collector():
         except TimeoutError:
             return False
 
+    def close_session(self) -> None:
+        self._driver.close()
+
+
+def run_the_scraper(search_string: str):
+    """
+    Main function to load the site and scrap recipe data based on a keyword search
+
+    Parameters
+    ----------
+    search_string : str
+        The ingredient, meal type or other relevant key word for the recipe search
+
+    """ 
+    my_scraper = recipe_collector(search_string)
+
+    try:
+
+        #execute the search
+        if my_scraper.search_recipes(search_string):
+            
+            for page in range(1, 2):
+                my_scraper.get_recipe_links(page)
+                #loop through urls gathered to get recipe details
+                #go to each item and scrape data
+
+            print(f"Found {len(my_scraper.recipe_urls)} recipes relating to {search_string}")
+
+            #create raw data folder
+            if not os.path.exists('raw_data'):
+                    os.mkdir('raw_data')
+
+            #create sub-folder for this search
+            data_folder = f'./raw_data/{search_string}'
+            if not os.path.exists(data_folder):
+                    os.mkdir(data_folder)
+
+            images_folder = f'./raw_data/{search_string}/images'
+            #create sub-folder for images
+            if not os.path.exists(images_folder):
+                    os.mkdir(images_folder)
+
+            all_recipes = {}
+
+            for idx, url in enumerate(my_scraper.recipe_urls):
+
+                if idx == 1:                                   
+                    #adding a break here so not looping through all during dev/test cycle
+                    break
+
+                recipe_dict = my_scraper.get_recipe_data(url)
+                all_recipes.update(recipe_dict)
+
+                file_name = recipe_dict['recipe_id']
+                image_url = recipe_dict['image_url']
+
+                # Download the file from `url` and save it locally under `file_name`:
+                urllib.request.urlretrieve(image_url, f"{images_folder}/{file_name}.jpg")
+
+            with open(f"{data_folder}/data.json", "w") as outfile:
+                json.dump(all_recipes, outfile, cls=UUIDEncoder, indent=4)
+
+        else:
+            print(f"There were no recipes relating to {search_string}")
+        
+    except Exception as e:
+        print(str(e))
+
+    my_scraper.close_session()
+
 if __name__ == "__main__":
-    my_scraper = recipe_collector("chicken")
+    run_the_scraper("chicken")
