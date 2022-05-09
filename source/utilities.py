cff083385e63bc33dba565615b9a7ff495fa5cea
ddbiasio <siobhanmdangelo@googlemail.com>
2022-05-09 19:42:17 +0100

    Removing file_ops to own class

--- source/utilities.py
+++ source/utilities.py
@@ -1,7 +1,5 @@
-import os
 import uuid
 import json
-import urllib.request
 
 class UUIDEncoder(json.JSONEncoder):
     """
@@ -24,44 +22,3 @@ class UUIDEncoder(json.JSONEncoder):
             return obj.hex
         return json.JSONEncoder.default(self, obj)
 
-class file_ops:
-
-    @staticmethod
-    def create_folder(folder_name: str):
-        """
-        Creates a folder if it doesn't exist already
-
-        Parameters
-        ----------
-        folder_name: str
-            The name of the folder to create (full path should be provided)
-
-        """
-        # create folder
-        if not os.path.exists(folder_name):
-            os.mkdir(folder_name)
-
-    @staticmethod
-    def dict_to_json_file(dict_to_save: dict, filename: str):
-        """
-        Creates a file in JSON format from a dictionary
-
-        Parameters
-        ----------
-        dict_to_save: dict
-            The dictionary object to be written to file
-        filename: str
-            The filename of the JSON file to be created
-        """
-        with open(f"{filename}", "w") as outfile:
-            json.dump(
-                dict_to_save,
-                outfile,
-                cls=UUIDEncoder,
-                indent=4)
-
-    @staticmethod
-    def get_image(url: str, file_name: str):
-
-        # Download the file from `url` and save it locally under `file_name`:
-        urllib.request.urlretrieve(url, f"{file_name}.jpg")
